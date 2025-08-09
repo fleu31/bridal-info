a41e4e63b15fe78061f607508088d79b2c7326dd
fleu31 <fleuvella@gmail.com>
2025-08-09 18:23:22 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,8 @@
-export default { experimental:{ typedRoutes:true } };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    // /studio/intent/edit/... など、/studio 以下はぜんぶ /studio を返す
+    return [{ source: '/studio/:path*', destination: '/studio' }]
+  },
+};
+module.exports = nextConfig;
