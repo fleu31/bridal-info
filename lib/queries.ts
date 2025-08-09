a4ddb97f718a0db0bb22efc047a62c40bb82ddcb
fleu31 <fleuvella@gmail.com>
2025-08-09 12:16:03 +0900

    Create queries.ts

---
+++ lib/queries.ts
@@ -0,0 +1,16 @@
+import {groq} from 'next-sanity'
+
+export const articlesListQuery = groq`
+  *[_type == "article"] | order(updatedAt desc) {
+    title, "slug": slug.current, category, updatedAt
+  }`
+
+export const articleBySlugQuery = groq`
+  *[_type == "article" && slug.current == $slug][0]{
+    title, "slug": slug.current, updatedAt, body
+  }`
+
+export const clinicsQuery = groq`
+  *[_type == "clinic"] | order(kana asc){
+    kana, name, area, labels, starts, concerns, dt, lastMinute, notes, url
+  }`
