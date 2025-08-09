5373e79b3c7871970c336a7079f9fce458d1ecce
fleu31 <fleuvella@gmail.com>
2025-08-09 12:14:30 +0900

    Create sanity.config.ts

---
+++ sanity.config.ts
@@ -0,0 +1,20 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import article from './schemas/article'
+import clinic from './schemas/clinic'
+import page from './schemas/page'
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-10-01'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Bridal CMS',
+  projectId,
+  dataset,
+  apiVersion,
+  basePath: '/studio',
+  plugins: [deskTool()],
+  schema: {types: [article, clinic, page]},
+})
