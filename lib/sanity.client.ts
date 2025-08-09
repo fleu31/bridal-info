fa6560c7c9842c6d1bc2aa79669e5db92fd0ef59
fleu31 <fleuvella@gmail.com>
2025-08-09 13:36:10 +0900

    Update sanity.client.ts

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,6 +1,8 @@
-import imageUrlBuilder from '@sanity/image-url'
-import { sanityClient } from './sanity.client'
-const builder = imageUrlBuilder(sanityClient)
-export function urlFor(src: any) {
-  return builder.image(src)
-}
+import {createClient} from '@sanity/client'
+
+export const sanityClient = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-10-01',
+  useCdn: true
+})
